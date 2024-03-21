ad6c32d48c3112b1ffcb941391896c841d0612f9
fadhil-khusnul <fadhilkh27@gmail.com>
2024-03-21 12:25:44 +0800

    pertemuanaddds

--- assets/js/pages/form.js
+++ assets/js/pages/form.js
@@ -108,7 +108,7 @@ console.log(tambah);
 
 let selectpertemuan = document.getElementById("pertemuan_ke[1]")
 
-for (var i = 1; i <= 50; i++) {
+for (var i = 1; i <= 100; i++) {
   var option = document.createElement("option");
   option.setAttribute("value", i);
   option.textContent = i;
@@ -143,8 +143,7 @@ async function tambah_kelas() {
 
 
   var select2 = document.createElement("select");
-  select2.innerHTML =
-    "<option selected disabled>Pilih Kelas</option>";
+ 
   select2.setAttribute("id", "id_kelas_kuliah[" + tambah + "]");
   select2.setAttribute("class", "js-states form-control");
   select2.setAttribute("style", "width: 100%");
@@ -239,7 +238,7 @@ async function tambah_kelas() {
   defaultOption.textContent = "Pilih Pertemuan Ke-";
   select3.appendChild(defaultOption);
 
-  for (var i = 1; i <= 50; i++) {
+  for (var i = 1; i <= 100; i++) {
     var option = document.createElement("option");
     option.setAttribute("value", i);
     option.textContent = i;
@@ -287,6 +286,7 @@ async function tambah_kelas() {
   var defaultOptionjenis3 = document.createElement("option");
   defaultOptionjenis3.setAttribute("value", "");
   defaultOptionjenis3.setAttribute("selected", true);
+  defaultOptionjenis3.setAttribute("disabled", true);
   defaultOptionjenis3.textContent = "Pilih Jenis Kendala"
   var defaultOptionjenis2 = document.createElement("option");
   defaultOptionjenis2.setAttribute("value", "PRESENSI DOUBLE/TERCONTRENG LEBIH DARI ANGKA 1 (ANGKA 2/3/4/etc..)");
@@ -414,6 +414,137 @@ function delete_container(r) {
 function SubmitForm() {
 
   console.log(tambah);
+  var form = $('#form_kendala');
+
+  // if (form.valid()) {
+  //   var submitBtn = document.getElementById('submitBtn');
+  //   var spinner = document.getElementById('spinner');
+  //   var btnText = document.getElementById('btnText');
+  //   var successAlert = document.getElementById('successAlert');
+  //   var errorAlert = document.getElementById('errorAlert');
+  
+  //   submitBtn.disabled = true; // Disable the button
+  //   spinner.classList.remove('d-none'); // Show the spinner
+  //   btnText.innerText = 'Loading...'; // Change the button text
+  
+  
+  //   const scriptURL = 'https://script.google.com/macros/s/AKfycbzg7bQyTf7QnbKNx5884tiM84gACK2FUlchfYlDYPM8q7DFTvV99vdi1xEbMnvngDBy/exec'
+  
+  
+  //   const formData = new FormData();
+  
+  //   var kode_mk = [];
+  //   var kode_mk_fullname = [];
+  //   var selectedOption_kode_mk_fullname = [];
+  //   var fullname_sikola = [];
+  
+  
+  //   var id_kelas_kuliah = [];
+  
+  //   var id_kelas_kuliah_fullname_kelas_sikola = [];
+  //   var selectedOption_fullname_kelas_sikola = [];
+  //   var fullname_kelas_sikola = [];
+  
+  
+  //   var pertemuan_ke = [];
+  //   var jenis_kendala = [];
+  //   var tanggal_terlaksana = [];
+  
+  //   for (var i = 0; i < tambah; i++) {
+  
+  //     kode_mk[i] = document.getElementById("kode_mk[" + (i + 1) + "]").value;
+  //     kode_mk_fullname[i] = document.getElementById("kode_mk[" + (i + 1) + "]");
+  //     selectedOption_kode_mk_fullname[i] = kode_mk_fullname[i].options[kode_mk_fullname[i].selectedIndex];
+  //     fullname_sikola[i] = selectedOption_kode_mk_fullname[i].getAttribute('fullname_sikola');
+  
+  //     id_kelas_kuliah[i] = document.getElementById("id_kelas_kuliah[" + (i + 1) + "]").value;
+  //     id_kelas_kuliah_fullname_kelas_sikola[i] = document.getElementById("id_kelas_kuliah[" + (i + 1) + "]");
+  //     selectedOption_fullname_kelas_sikola[i] = id_kelas_kuliah_fullname_kelas_sikola[i].options[id_kelas_kuliah_fullname_kelas_sikola[i].selectedIndex];
+  //     fullname_kelas_sikola[i] = selectedOption_fullname_kelas_sikola[i].getAttribute('fullname_kelas_sikola');
+  
+  
+  //     pertemuan_ke[i] = document.getElementById("pertemuan_ke[" + (i + 1) + "]").value;
+  //     jenis_kendala[i] = document.getElementById("jenis_kendala[" + (i + 1) + "]").value;
+  //     tanggal_terlaksana[i] = document.getElementById("tanggal_terlaksana[" + (i + 1) + "]").value;
+  
+  //     formData.append('kode_mk[' + i + ']', kode_mk[i]);
+  //     formData.append('id_kelas_kuliah[' + i + ']', id_kelas_kuliah[i]);
+  //     formData.append('pertemuan_ke[' + i + ']', pertemuan_ke[i]);
+  //     formData.append('jenis_kendala[' + i + ']', jenis_kendala[i]);
+  //     formData.append('tanggal_terlaksana[' + i + ']', tanggal_terlaksana[i]);
+  
+      
+  
+  //     formData.append('fullname_sikola[' + i + ']', fullname_sikola[i]);
+  //     formData.append('fullname_kelas_sikola[' + i + ']', fullname_kelas_sikola[i]);
+  
+  //   }
+  
+  //   console.log(fullname_sikola);
+  
+  //   const programStudiSelect = document.getElementById("program_studi");
+  //   const selectedOption = programStudiSelect.options[programStudiSelect.selectedIndex];
+  //   const fakultas = selectedOption.getAttribute('data_fakultas');
+  
+  //   let program_studi = document.getElementById("program_studi").value;
+  //   // let keterangan_kendala = document.getElementById("keterangan_kendala").value;
+  //   let no_wa = document.getElementById("no_wa").value;
+  //   no_wa = no_wa.replace(/[\s\-+]/g, '');
+  //   no_wa = 'wa.me/'+ no_wa;
+  
+  //   console.log(fakultas, program_studi, no_wa);
+  
+  //   formData.append("fakultas", fakultas);
+  //   formData.append("program_studi", program_studi);
+  //   formData.append("no_wa", no_wa);
+  //   // formData.append("keterangan_kendala", keterangan_kendala);
+  //   formData.append("tambah", tambah);
+  
+  
+  
+  //   fetch(scriptURL, { method: 'POST', body: formData })
+  //     .then(response => {
+  //       submitBtn.disabled = false; // Enable the button
+  //       spinner.classList.add('d-none'); // Hide the spinner
+  //       btnText.innerText = 'Submit'; // Reset the button text
+  
+  //       if (response.ok) {
+  
+  //         errorAlert.classList.add('d-none');
+  
+  //         localStorage.setItem('formSubmitted', 'true');
+  
+  //         $.blockUI({
+  //           message: '<div class="spinner-grow text-primary" role="status"><span class="visually-hidden">Loading...</span><div>',
+  //           timeout: 5000
+  //         });
+  
+  //         setTimeout(() => {
+  //           window.location.reload();
+  //         }, 5000);
+  
+  //       } else {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //     })
+  //     .catch(error => {
+  //       var error_msg = document.getElementById("error_msg");
+  
+  //       error_msg.innerText = error
+  //       submitBtn.disabled = false; // Enable the button
+  //       spinner.classList.add('d-none'); // Hide the spinner
+  //       btnText.innerText = 'Submit';
+  
+  
+  //       successAlert.classList.add('d-none'); // Hide success alert
+  //       errorAlert.classList.remove('d-none'); // Show error alert
+  
+  //       console.error('Error!', error.message);
+  //     });
+  //   return true; // Allow the form to submit
+  // } else {
+  //   return false; // Prevent the form from submitting
+  // }
 
   var submitBtn = document.getElementById('submitBtn');
   var spinner = document.getElementById('spinner');
@@ -539,14 +670,14 @@ function SubmitForm() {
 
       console.error('Error!', error.message);
     });
+  return false;
 
 
 
 
+  
 
 
-
-  return false; // Prevent the form from submitting
 
 }
 
@@ -557,5 +688,7 @@ $('#no_wa').inputmask({
   clearMaskOnLostFocus: false,
   
 });
+
+
 
 
